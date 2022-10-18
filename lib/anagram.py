2dc1df282f9fd33e800484b2157b38124242f783
Monica Gerard <mlgvla@gmail.com>
2022-10-18 11:05:39 -0400

    solution

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = "".join(sorted(word.lower()))
+
+    def match(self, list):
+        anagrams = []
+
+        for w in list:
+            sorted_w = "".join(sorted(w.lower()))
+            if sorted_w == self.sorted_word:
+                anagrams.append(w)
+        
+        return anagrams
